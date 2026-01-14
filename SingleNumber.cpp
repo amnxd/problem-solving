598ee13921c3b07d31e2721ca4ae4b8788cc16e9
Aman <bhaskarshah4235@gmail.com>
2026-01-15 01:04:38 +0530

    'temp'

---
+++ SingleNumber.cpp
@@ -0,0 +1,16 @@
+#import<bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    int result = 0;
+    for (int num : nums) {
+        result ^= num;
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+    cout << "The single number is: " << singleNumber(nums) << endl;
+    return 0;
+}
---
+++ cpp/fibonacci.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int inputt;
+    
+    cout<<"Enter the number of elements: ";
+    cin>>inputt;
+    int c = inputt;
+    
+    int arr[inputt];
+    int arr2[inputt];
+    
+    for(int i=0; i<inputt; i++){
+        arr[i] = c;
+        c++;
+    }
+    
+    
+    return 0;
+}
